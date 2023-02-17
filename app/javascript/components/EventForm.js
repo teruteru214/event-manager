a7329c374ee1c12555a4753724b14d2d7b7fa035
teruteru <tutituti3080@gmail.com>
2023-02-17 14:09:50 +0900

    Add: pikadayを実装

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { isEmptyObject, validateEvent } from '../helpers/helpers';
+import React, { useState, useRef, useEffect } from 'react';
+import Pikaday from 'pikaday';
+import 'pikaday/css/pikaday.css';
+import { formatDate, isEmptyObject, validateEvent } from '../helpers/helpers';
 
 const EventForm = () => {
   const [event, setEvent] = useState({
@@ -13,12 +15,31 @@ const EventForm = () => {
 
   const [formErrors, setFormErrors] = useState({});
 
+  const dateInput = useRef(null);
+
+  useEffect(() => {
+    const p = new Pikaday({
+      field: dateInput.current,
+      onSelect: (date) => {
+        const formattedDate = formatDate(date);
+        dateInput.current.value = formattedDate;
+        updateEvent('event_date', formattedDate);
+      },
+    });
+
+    return () => p.destroy();
+  }, []);
+
+  const updateEvent = (key, value) => {
+    setEvent((prevEvent) => ({ ...prevEvent, [key]: value }));
+  };
+
   const handleInputChange = (e) => {
     const { target } = e;
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
-    setEvent({ ...event, [name]: value });
+    updateEvent(name, value);
   };
 
   const renderErrors = () => {
@@ -67,15 +88,18 @@ const EventForm = () => {
           </label>
         </div>
         <div>
-          <label htmlFor="event_date">
-            <strong>Date:</strong>
-            <input
-              type="text"
-              id="event_date"
-              name="event_date"
-              onChange={handleInputChange}
-            />
-          </label>
+          <div>
+            <label htmlFor="event_date">
+              <strong>Date:</strong>
+              <input
+                type="text"
+                id="event_date"
+                name="event_date"
+                ref={dateInput}
+                autoComplete="off"
+              />
+            </label>
+          </div>
         </div>
         <div>
           <label htmlFor="title">
