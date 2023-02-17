5898ad0eb2953afd161956c0f9d1602e1a3646d4
teruteru <tutituti3080@gmail.com>
2023-02-17 12:06:05 +0900

    initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,17 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+});
---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Editor from './Editor';
+import './App.css';
+
+const App = () => (
+  <Routes>
+    <Route path="events/*" element={<Editor />} />
+  </Routes>
+);
+
+
+export default App;
